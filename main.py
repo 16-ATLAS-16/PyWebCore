b327aeeaf06a1f4eb8ce3f846bbce088a4503110
16-ATLAS-16 <theatlas161616@gmail.com>
2025-04-29 21:42:41 +0100

    added favico

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='./static')
 for route in os.listdir('./routes'):
     if route.endswith('.py'):
         routeName = route.strip('.py')
         regCmd = f'from routes.{routeName} import bp as {routeName}_bp\napp.register_blueprint({routeName}_bp)'
         exec(regCmd)
         print('Registered route: ', routeName)
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 app.run()
