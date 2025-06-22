7a0982e01d52b22e5924e9b25cac4eddc3c2124a
16-ATLAS-16 <theatlas161616@gmail.com>
2025-06-22 18:58:01 +0100

    Small navbar changes

--- routes/test.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-
-bp = Blueprint('test', __name__, url_prefix='/test', template_folder='../templates')
-bp.navbar_visible = True
-
-@bp.route('/')
-def home():
-    return "<p>test!!</p>"
